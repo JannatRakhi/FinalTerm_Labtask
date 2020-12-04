b6a079fb1da7f9bfdaa7324d7ce31e1fef3e19f9
Jannat Rakhi <jannatrakhi31155@gmail.com>
2020-12-04 21:48:06 +0600

    Completed

---
+++ Test_Interface_1/Test_Interface_1/Calculation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_1
+{
+    class Calculation : ScientificCalculatorInterface, BasicCalculatorInterface
+    {
+
+        public int XtoY(int x, int y)
+        {
+
+            return Convert.ToInt32(Math.Pow(x, y));
+
+        }
+
+
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+
+
+        public int sub(int x, int y)
+        {
+            return x - y;
+        }
+
+
+        public int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+
+        public int division(int x, int y)
+        {
+            return x / y;
+        }
+
+    }
+}
---
+++ Test_Interface_1/Test_Interface_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calculation = new Calculation();
+            int x = 20;
+            int y = 5;
+            Console.WriteLine("The value of x & y are: " + x + " and " + y + "\n");
+            Console.WriteLine(x + " Power " + y + " = " + calculation.XtoY(x, y));
+            Console.WriteLine(x + " + " + y + " = " + calculation.sum(x, y));
+            Console.WriteLine(x + " - " + y + " = " + calculation.sub(x, y));
+            Console.WriteLine(x + " * " + y + " = " + (calculation.multiplication(x, y)));
+            Console.WriteLine(x + " / " + y + " = " + calculation.division(x, y));
+
+
+            Console.ReadLine();
+        }
+    }
+}
